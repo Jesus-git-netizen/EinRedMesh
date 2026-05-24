4e91a58bbf7e7c49d3b3156fa7bcf9a106108a27
Jesus <jesusguadalupe762@gmail.com>
2026-05-23 20:58:25 -0600

    sas

---
+++ EinRedMesh.Core/Generacion/GeneracionEntity.cs
@@ -0,0 +1,16 @@
+﻿using Ein.Dtos;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Ein.Entidades
+{
+    [Table("Generacion")]
+    public class GeneracionEntity
+    {
+       
+
+        [Key]public int Id { get; set; }
+        [Required, StringLength(12)]public string Nombre { get; set; } =string.Empty;
+        [Required]public bool EstaActivo { get; set; }
+    }
+}
